4739fffadfb4abf564956ec302ae97c5de106fd6
sh <sh@users.noreply.github.com>
2018-10-31 10:24:29 +0700

    fix mongodb host

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
+console.log("env", process.env);
 module.exports = {
   app: {
     port: process.env.PORT || 8000
   },
   db: {
-    host: process.env.MONGODB_HOST,
+    host: "mongodb://" + process.env.MONGODB_HOST,
     port: process.env.MONGODB_PORT,
     name: process.env.MONGODB_NAME
   },
