6c77d7d42e2a26cef7f25944a3074244b7b9b144
sh <sh@users.noreply.github.com>
2018-10-31 09:57:03 +0700

    unify priceApi config

--- server/config/config.js
+++ server/config/config.js
@@ -1,70 +1,56 @@
 const env = process.env.NODE_ENV;
 
+const priceApi = {
+  endpoint: "https://api.priceapi.com/v2/jobs",
+  token: "TMMQFUMKMDRXICUZYTXAFFTEESNKMUOGSPEKVNMVOCEGSTXLYRCQBVAFETUCHDKB",
+  source: "google_shopping",
+  country: "de",
+  topic: "search_results",
+  key: "gtin",
+  max_pages: 3
+};
+
 const dev = {
-	app: {
-		port: 8000
-	},
-	db: {
-		host: 'mongodb://localhost',
-		port: 27017,
-		name: 'fweha1',
-	},
-	priceAPI: {
-		endpoint: 'https://api.priceapi.com/v2/jobs',
-		token: 'TMMQFUMKMDRXICUZYTXAFFTEESNKMUOGSPEKVNMVOCEGSTXLYRCQBVAFETUCHDKB',
-		source: 'google_shopping',
-		country: 'de',
-		topic: 'search_results',
-		key: 'gtin',
-		max_pages: 3
-	}
+  app: {
+    port: 8000
+  },
+  db: {
+    host: "mongodb://localhost",
+    port: 27017,
+    name: "fweha1"
+  },
+  priceAPI
 };
 
 const test = {
-	app: {
-		port: 8000
-	},
-	db: {
-		host: 'mongodb://localhost',
-		port: 27017,
-		name: 'fweha1_test',
-	},
-	priceAPI: {
-		endpoint: 'https://api.priceapi.com/v2/jobs',
-		token: 'TMMQFUMKMDRXICUZYTXAFFTEESNKMUOGSPEKVNMVOCEGSTXLYRCQBVAFETUCHDKB',
-		source: 'google_shopping',
-		country: 'de',
-		topic: 'search_results',
-		key: 'gtin',
-		max_pages: 3
-	}
+  app: {
+    port: 8000
+  },
+  db: {
+    host: "mongodb://localhost",
+    port: 27017,
+    name: "fweha1_test"
+  },
+  priceAPI
 };
 
 const live = {
-	app: {
-		port: 8000
-	},
-	db: {
-		host: 'mongodb://localhost',
-		port: 27017,
-		name: 'fweha1_live',
-		collection: 'products'
-	},
-	priceAPI: {
-		endpoint: 'https://api.priceapi.com/v2/jobs',
-		token: 'TMMQFUMKMDRXICUZYTXAFFTEESNKMUOGSPEKVNMVOCEGSTXLYRCQBVAFETUCHDKB',
-		source: 'google_shopping',
-		country: 'de',
-		topic: 'search_results',
-		key: 'gtin',
-		max_pages: 3
-	}
+  app: {
+    port: 8000
+  },
+  db: {
+    host: "mongodb://localhost",
+    port: 27017,
+    name: "fweha1_live",
+    collection: "products"
+  },
+  priceAPI
 };
 
 const config = {
-	dev,
-	test,
-	live
+  dev,
+  test,
+  live
 };
 
 module.exports = config[env];
